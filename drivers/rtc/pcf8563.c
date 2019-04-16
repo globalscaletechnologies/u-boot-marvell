8d1b4a59092694e6f0c8bd907b32a05d8f2640e0
Jason Hung <jhung@globalscaletechnologies.com>
2019-04-16 13:53:06 +0800

    rtc: pfc8563: add support pcf8563 rtc driver support
    add DM driver support for pcf8563 chip to instead of original
    rtc_get / rtc_set function.

--- drivers/rtc/pcf8563.c
+++ drivers/rtc/pcf8563.c
@@ -13,110 +13,147 @@
 
 #include <common.h>
 #include <command.h>
-#include <rtc.h>
+#include <dm.h>
 #include <i2c.h>
+#include <rtc.h>
 
-#if defined(CONFIG_CMD_DATE)
-
-static uchar rtc_read  (uchar reg);
-static void  rtc_write (uchar reg, uchar val);
-
-/* ------------------------------------------------------------------------- */
-
-int rtc_get (struct rtc_time *tmp)
+static int pcf8563_rtc_set(struct udevice *dev, const struct rtc_time *tmp)
 {
-	int rel = 0;
-	uchar sec, min, hour, mday, wday, mon_cent, year;
-
-	sec	= rtc_read (0x02);
-	min	= rtc_read (0x03);
-	hour	= rtc_read (0x04);
-	mday	= rtc_read (0x05);
-	wday	= rtc_read (0x06);
-	mon_cent= rtc_read (0x07);
-	year	= rtc_read (0x08);
-
-	debug ( "Get RTC year: %02x mon/cent: %02x mday: %02x wday: %02x "
-		"hr: %02x min: %02x sec: %02x\n",
-		year, mon_cent, mday, wday,
-		hour, min, sec );
-	debug ( "Alarms: wday: %02x day: %02x hour: %02x min: %02x\n",
-		rtc_read (0x0C),
-		rtc_read (0x0B),
-		rtc_read (0x0A),
-		rtc_read (0x09) );
-
-	if (sec & 0x80) {
-		puts ("### Warning: RTC Low Voltage - date/time not reliable\n");
-		rel = -1;
-	}
-
-	tmp->tm_sec  = bcd2bin (sec  & 0x7F);
-	tmp->tm_min  = bcd2bin (min  & 0x7F);
-	tmp->tm_hour = bcd2bin (hour & 0x3F);
-	tmp->tm_mday = bcd2bin (mday & 0x3F);
-	tmp->tm_mon  = bcd2bin (mon_cent & 0x1F);
-	tmp->tm_year = bcd2bin (year) + ((mon_cent & 0x80) ? 1900 : 2000);
-	tmp->tm_wday = bcd2bin (wday & 0x07);
-	tmp->tm_yday = 0;
-	tmp->tm_isdst= 0;
-
-	debug ( "Get DATE: %4d-%02d-%02d (wday=%d)  TIME: %2d:%02d:%02d\n",
-		tmp->tm_year, tmp->tm_mon, tmp->tm_mday, tmp->tm_wday,
-		tmp->tm_hour, tmp->tm_min, tmp->tm_sec);
-
-	return rel;
-}
-
-int rtc_set (struct rtc_time *tmp)
-{
-	uchar century;
+	uchar buf[1], century;
+	int  ret;
 
 	debug ( "Set DATE: %4d-%02d-%02d (wday=%d)  TIME: %2d:%02d:%02d\n",
 		tmp->tm_year, tmp->tm_mon, tmp->tm_mday, tmp->tm_wday,
 		tmp->tm_hour, tmp->tm_min, tmp->tm_sec);
-
-	rtc_write (0x08, bin2bcd(tmp->tm_year % 100));
+	/* start register address */
+	buf[0] = bin2bcd(tmp->tm_year % 100);
+	ret = dm_i2c_write(dev, 0x08, buf, 1);
+	if (ret < 0)
+		return ret;
 
 	century = (tmp->tm_year >= 2000) ? 0 : 0x80;
-	rtc_write (0x07, bin2bcd(tmp->tm_mon) | century);
+	buf[0] = bin2bcd(tmp->tm_mon) | century;
+	ret = dm_i2c_write(dev, 0x07, buf, 1);
+	if (ret < 0)
+		return ret;
 
-	rtc_write (0x06, bin2bcd(tmp->tm_wday));
-	rtc_write (0x05, bin2bcd(tmp->tm_mday));
-	rtc_write (0x04, bin2bcd(tmp->tm_hour));
-	rtc_write (0x03, bin2bcd(tmp->tm_min ));
-	rtc_write (0x02, bin2bcd(tmp->tm_sec ));
+	buf[0] = bin2bcd(tmp->tm_wday);
+	ret = dm_i2c_write(dev, 0x06, buf, 1);
+	if (ret < 0)
+		return ret;
+
+	buf[0] = bin2bcd(tmp->tm_mday);
+	ret = dm_i2c_write(dev, 0x5, buf, 1);
+	if (ret < 0)
+		return ret;
+
+	buf[0] = bin2bcd(tmp->tm_hour);
+	ret = dm_i2c_write(dev, 0x4, buf, 1);
+	if (ret < 0)
+		return ret;
+
+	buf[0] = bin2bcd(tmp->tm_min);
+	ret = dm_i2c_write(dev, 0x3, buf, 1);
+	if (ret < 0)
+		return ret;
+
+	buf[0] = bin2bcd(tmp->tm_sec);
+	ret = dm_i2c_write(dev, 0x2, buf, 1);
+	if (ret < 0)
+		return ret;
+
+	return ret;
+}
+
+static int pcf8563_rtc_get(struct udevice *dev, struct rtc_time *tmp)
+{
+	int i, ret = 0;
+	uchar buf[12];
+	uchar *p = buf;
+	for(i=2;i<0x0d;i++){
+		ret = dm_i2c_read(dev, i, p, 1);
+		if (ret < 0)
+			return ret;
+		p++;
+	}
+
+	debug ( "Get RTC year: %02x mon/cent: %02x mday: %02x wday: %02x "
+		"hr: %02x min: %02x sec: %02x\n",
+		buf[6], buf[5], buf[3], buf[4],buf[2], buf[1], buf[0] );
+	debug ( "Alarms: wday: %02x day: %02x hour: %02x min: %02x\n",
+		buf[10],buf[9],buf[8],buf[7]);
+	if (buf[0] & 0x80) {
+		puts ("### Warning: RTC Low Voltage - date/time not reliable\n");
+		ret = -1;
+	}
+
+	tmp->tm_sec  = bcd2bin (buf[0]  & 0x7F);
+	tmp->tm_min  = bcd2bin (buf[1]  & 0x7F);
+	tmp->tm_hour = bcd2bin (buf[2] & 0x3F);
+	tmp->tm_mday = bcd2bin (buf[3] & 0x3F);
+	tmp->tm_mon  = bcd2bin (buf[5] & 0x1F);
+	tmp->tm_year = bcd2bin (buf[6]) + ((buf[5] & 0x80) ? 1900 : 2000);
+	tmp->tm_wday = bcd2bin (buf[4] & 0x07);
+	tmp->tm_yday = 0;
+	tmp->tm_isdst= 0;
+
+	debug("Get DATE: %4d-%02d-%02d (wday=%d)  TIME: %2d:%02d:%02d\n",
+	      tmp->tm_year, tmp->tm_mon, tmp->tm_mday, tmp->tm_wday,
+	      tmp->tm_hour, tmp->tm_min, tmp->tm_sec);
+
+	return ret;
+}
+
+static int pcf8563_rtc_reset(struct udevice *dev)
+{
+	uchar buf[1];
+	int  i, ret;
+	/* clear all control & status registers */
+	buf[0] = 0x00;
+	ret = dm_i2c_write(dev, 0, buf, 1);
+	if (ret < 0)
+		return ret;
+	ret = dm_i2c_write(dev, 1, buf, 1);
+	if (ret < 0)
+		return ret;
+	ret = dm_i2c_write(dev, 0x0D, buf, 1);
+	if (ret < 0)
+		return ret;
+
+	/* clear Voltage Low bit */
+	ret = dm_i2c_read(dev, 0x02, buf, 1);
+	if (ret < 0)
+		return ret;
+	buf[0] &= 0x7F;
+	ret = dm_i2c_write(dev, 0x02, buf, 1);
+	if (ret < 0)
+		return ret;
+
+	/* reset all alarms */
+	buf[0] = 0x00;
+	for(i=0x09;i<0x0d;i++){
+		ret = dm_i2c_write(dev, i, buf, 1);
+		if (ret < 0)
+			return ret;
+	}
 
 	return 0;
 }
 
-void rtc_reset (void)
-{
-	/* clear all control & status registers */
-	rtc_write (0x00, 0x00);
-	rtc_write (0x01, 0x00);
-	rtc_write (0x0D, 0x00);
+static const struct rtc_ops pcf8563_rtc_ops = {
+	.get = pcf8563_rtc_get,
+	.set = pcf8563_rtc_set,
+	.reset = pcf8563_rtc_reset,
+};
 
-	/* clear Voltage Low bit */
-	rtc_write (0x02, rtc_read (0x02) & 0x7F);
+static const struct udevice_id pcf8563_rtc_ids[] = {
+	{ .compatible = "nxp,pcf8563" },
+	{ }
+};
 
-	/* reset all alarms */
-	rtc_write (0x09, 0x00);
-	rtc_write (0x0A, 0x00);
-	rtc_write (0x0B, 0x00);
-	rtc_write (0x0C, 0x00);
-}
-
-/* ------------------------------------------------------------------------- */
-
-static uchar rtc_read (uchar reg)
-{
-	return (i2c_reg_read (CONFIG_SYS_I2C_RTC_ADDR, reg));
-}
-
-static void rtc_write (uchar reg, uchar val)
-{
-	i2c_reg_write (CONFIG_SYS_I2C_RTC_ADDR, reg, val);
-}
-
-#endif
+U_BOOT_DRIVER(rtc_pcf8563) = {
+	.name	= "rtc-pcf8563",
+	.id	= UCLASS_RTC,
+	.of_match = pcf8563_rtc_ids,
+	.ops	= &pcf8563_rtc_ops,
+};
