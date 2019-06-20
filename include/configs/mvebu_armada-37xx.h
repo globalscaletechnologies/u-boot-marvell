15d513e77fd5820f560608ed1d15e737613a5bff
Jason Hung <jhung@globalscaletechnologies.com>
2019-06-20 14:06:59 +0800

    env: sheeva64: modify default env setting value for autoboot
    modify default env setting value for autoboot, the default
    setting will boot from emmc partition.

--- include/configs/mvebu_armada-37xx.h
+++ include/configs/mvebu_armada-37xx.h
@@ -48,10 +48,53 @@
 
 
 #undef BOOT_TARGET_DEVICES
+#undef CONFIG_EXTRA_ENV_SETTINGS
+
+#define SDBOOT		\
+					"sdboot=mmc dev 0; " \
+					"ext4load mmc 0:1 $kernel_addr_r $image_name; " \
+					"ext4load mmc 0:1 $fdt_addr_r $fdt_name; " \
+					"setenv bootargs $console root=/dev/mmcblk0p1 rw rootwait " \
+					"net.ifnames=0 biosdevname=0 $extra_params; " \
+					"booti $kernel_addr_r - $fdt_addr_r\0"
+
+#define EMMCBOOT	\
+					"emmcboot=mmc dev 1; " \
+					"ext4load mmc 1:1 $kernel_addr_r $image_name; " \
+					"ext4load mmc 1:1 $fdt_addr_r $fdt_name; " \
+					"setenv bootargs $console " \
+					"root=PARTUUID=89708921-01 rw rootwait " \
+					"net.ifnames=0 biosdevname=0 $extra_params; " \
+					"booti $kernel_addr_r - $fdt_addr_r\0"
+
+#define FACTORYBOOT	\
+					"recovery=usb reset; " \
+					"fatload usb 0 $loadaddr sheeva64-recovery.bin; " \
+					"source $loadaddr\0"
+
 #undef BOOTENV
-#define BOOTENV 				"\0"
+#define BOOTENV		\
+					EMMCBOOT \
+					SDBOOT \
+					FACTORYBOOT
+
+#define CONFIG_EXTRA_ENV_SETTINGS		\
+	"fdt_name=boot/" CONFIG_DEFAULT_DEVICE_TREE ".dtb\0"		\
+	"image_name=boot/Image\0"			\
+	"kernel_addr_r=0x7000000\0"		\
+	"initrd_addr=0xa00000\0"		\
+	"initrd_size=0x2000000\0"		\
+	"fdt_addr_r=0x6f00000\0"		\
+	"loadaddr=0x7000000\0"			\
+	"fdt_high=0xffffffffffffffff\0"		\
+	"hostname=marvell\0"			\
+	"netdev=eth0\0"				\
+	"ethaddr=00:51:82:11:22:00\0"		\
+	"eth1addr=00:51:82:11:22:01\0"		\
+	"console=" CONFIG_DEFAULT_CONSOLE "\0"	\
+	BOOTENV
 
 #undef CONFIG_BOOTCOMMAND
-#define CONFIG_BOOTCOMMAND		"run bootcmd_nfs"
+#define CONFIG_BOOTCOMMAND		"run emmcboot"
 
 #endif /* _CONFIG_MVEBU_ARMADA_37XX_H */
